a7e601d4883480f4eb39a7d7faadf58864053ff1
nik <nikun2008@126.com>
2023-03-16 12:00:36 +0800

    Initial commit

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,23 @@
+import {defineUserConfig,defaultTheme} from 'vuepress'
+
+export default defineUserConfig({
+	lang: 'zh-CN',
+	title: '你好',
+	description: '个人博客',
+	theme: defaultTheme({
+		// public文件路径
+		logo: '/logo.png',
+		// 默认主题配置
+		navbar: [
+			{
+				text: '首页',
+				link: '/',
+			},
+			{
+				text: '项目',
+				link: '/projects',
+			},
+		],
+		repo: 'https://github.com/montilloo/montilloo.github.io',
+	})
+})
