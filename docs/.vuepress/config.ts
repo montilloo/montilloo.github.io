d5ee83ebde5fa90178055c9836f50cec66f04030
nik <nikun2008@126.com>
2023-03-22 10:27:21 +0800

    deploy;

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,5 +1,7 @@
-import {defineUserConfig,defaultTheme} from 'vuepress'
-
+import {defineUserConfig,defaultTheme} from 'vuepress';
+import {backToTopPlugin} from '@vuepress/plugin-back-to-top';
+import {registerComponentsPlugin} from '@vuepress/plugin-register-components'
+import {docsearchPlugin} from "@vuepress/plugin-docsearch";
 export default defineUserConfig({
 	lang: 'zh-CN',
 	title: '',
@@ -16,9 +18,30 @@ export default defineUserConfig({
 			},
 			{
 				text: '项目',
-				link: '/projects',
+				children: [
+					{
+						text: '德善教育',
+						link: '/projects/deshan',
+					},
+				],
 			},
+			{
+				text: 'CSS效果合集',
+				link: '/projects/css'
+			}
 		],
 		repo: 'https://github.com/montilloo/montilloo.github.io',
-	})
+	}),
+	plugins: [
+		backToTopPlugin(),
+		registerComponentsPlugin({
+			// 配置项
+		}),
+		docsearchPlugin({
+			appId: process.env.appId,
+			apiKey: process.env.apiKey,
+			indexName: process.env.indexName,
+			placeholder: '搜索...',
+		}),
+	]
 })
---
+++ docs/projects/css/README.md
@@ -0,0 +1,5 @@
+# CSS合集
+
+1. [iPad风格的光标跟随CSS锚定&&：has（）](https://html5-boilerplate.nikun.me)
+* fads 
+*
